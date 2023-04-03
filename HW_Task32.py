1748a9963ec1b1ef3bb33c078e71b34176ea9b1b
rusyun8 <rustam.yunusov@attolabs.de>
2023-04-03 20:46:03 +0500

    update HW Task 32

--- HW_Task32.py
+++ HW_Task32.py
@@ -2,10 +2,10 @@
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
 
 list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
-min_number = int(input())
-max_number = int(input())
+low_point = int(input())
+top_point = int(input())
 list_2 = []
 for i in range(len(list_1)):
-    if min_number <= list_1[i] <= max_number:
+    if low_point <= list_1[i] <= top_point:
         list_2.append(i)
 print(list_2)
