c577f02f46805926f9ddbcbd1b3ccb0c0b8d124d
rusyun8 <rustam.yunusov@attolabs.de>
2023-04-10 17:01:59 +0500

    update Task 51

--- Task51.py
+++ Task51.py
@@ -11,10 +11,17 @@
 #     print(‘different’)
 
 def same_by(characteristic, objects):
-    for i in range(1, len(objects)):
-        if characteristic(objects[i]) != characteristic(objects[i-1]):
-            return False
-    return True
+    # for i in range(1, len(objects)):
+    #     if characteristic(objects[i]) != characteristic(objects[i-1]):
+    #         return False
+    # return True
+    print(objects)
+    map_obj = list(map(characteristic, objects))
+    print(map_obj)
+    print(set(map_obj))
+    print(len(set(map_obj)))
+    print(len(set(map(characteristic, objects))) == 1)
+    return len(set(map(characteristic, objects))) == 1
 
 values = [0, 2, 10, 6]
 if same_by(lambda x: x % 2, values):
